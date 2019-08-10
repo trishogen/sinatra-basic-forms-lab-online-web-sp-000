f618beb2eef5b3ed73ad0d8b8338f17c4bc993e3
Trish Ogen <patriciaogen@gmail.com>
2019-08-10 23:26:13 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,5 +1,4 @@
 class Puppy
-
   attr_accessor :name, :breed, :months_old
 
   def initalize(name, breed, months_old)
@@ -7,5 +6,4 @@ class Puppy
     @breed = breed
     @months_old = months_old
   end
-
 end
