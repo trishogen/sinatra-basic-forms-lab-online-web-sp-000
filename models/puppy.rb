31975ebc2e278e0608d1e8e0db67036919138c7e
Trish Ogen <patriciaogen@gmail.com>
2019-08-10 23:19:57 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,5 +1,9 @@
 class Puppy
-
-  def initalize
+  attr_accessor :name, :breed, :months_old
+  
+  def initalize(name, breed, months_old)
+    @name = name
+    @breed = breed
+    @months_old = months_old
   end
 end
